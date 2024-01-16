8a1d7784b505aceab92da898fa70761c08fee4e0
daksh-goyal7 <149812584+daksh-goyal7@users.noreply.github.com>
2024-01-16 14:43:53 +0530

    Add files via upload

---
+++ resturant_menu/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+MEAL_TYPE = (("starters","Starters"),
+           ("salads","Salads"),
+           ("main_dishes","Main Dishes"),
+           ("desserts","Desserts")
+           )
+
+STATUS = ((0,"unavailable"),
+        (1,"available")
+        )
+
+
+class Item(models.Model):
+    meal=models.CharField(max_length=1000,unique=True)
+    description=models.CharField(max_length=2000)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    meal_type=models.CharField(max_length=1000,choices=MEAL_TYPE)
+    author=models.ForeignKey(User,on_delete=models.PROTECT)
+    status=models.IntegerField(choices=STATUS,default=0)
+    date_created=models.DateTimeField(auto_now_add=True)
+    date_edited=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
